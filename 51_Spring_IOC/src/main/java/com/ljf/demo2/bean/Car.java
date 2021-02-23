8f33a9956f15ec05757ee35024ae22bdd7da6bb0
ljf <ljfsunlight@gmail.com>
2021-02-23 16:27:35 +0800

    引入pom并添加实体Car

---
+++ 51_Spring_IOC/src/main/java/com/ljf/demo2/bean/Car.java
@@ -0,0 +1,15 @@
+package com.ljf.demo2.bean;
+
+import lombok.Data;
+
+/**
+ * @author: ljf
+ * @create: 2021-02-23 16:20
+ * @description:
+ **/
+@Data
+public class Car {
+    private String name;
+    private String color;
+    private String price;
+}
