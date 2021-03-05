ff55a30fab51f498a351ddef13a964a315294c1b
ljf <ljfsunlight@gmail.com>
2021-03-05 13:53:41 +0800

    Spring Boot基础搭建

---
+++ 70_SpringBoot/src/main/java/com/ljf/demo/ModifyApplication.java
@@ -0,0 +1,34 @@
+package com.ljf.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModifyApplication {
+
+    public static void main(String[] args) {
+
+        //SpringApplication application = new SpringApplication(ModifyApplication.class);
+
+        //application.setBannerMode(Banner.Mode.OFF); 控制项目启动图案的显示和隐藏
+
+        //application.run(args);
+
+        SpringApplication.run(ModifyApplication.class, args);
+
+
+    }
+}
+
+/**
+ * 常用配置:
+ *
+ *
+ *
+ *
+ */
+
+
+
+
