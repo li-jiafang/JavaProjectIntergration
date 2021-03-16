6e76d986d859b3d0206f06d2942179e5bcf1dc0d
ljf <ljfsunlight@gmail.com>
2021-03-16 16:41:15 +0800

    mybatis学习

---
+++ 60_Mybatis/src/main/java/com/ljf/bean/Emp.java
@@ -0,0 +1,15 @@
+package com.ljf.bean;
+
+/**
+ * @author: ljf
+ * @create: 2021-03-16 16:29
+ * @description:
+ **/
+
+public class Emp {
+    private int id;
+    private String name;
+    private byte age;
+    private int deptId;
+    private int empno;
+}
---
+++ 60_Mybatis/src/main/java/com/ljf/dao/EmpDao.java
@@ -0,0 +1,11 @@
+package com.ljf.dao;
+
+/**
+ * @author: ljf
+ * @create: 2021-03-16 16:28
+ * @description:
+ **/
+public interface EmpDao {
+
+    void getList();
+}
