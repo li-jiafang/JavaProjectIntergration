c9f3feab2eba39157e3cc2ecc8d590ed5ed12204
ljf <ljfsunlight@gmail.com>
2021-03-16 16:41:33 +0800

    mybatis学习

--- 60_Mybatis/src/main/java/com/ljf/bean/Emp.java
+++ 60_Mybatis/src/main/java/com/ljf/bean/Emp.java
@@ -1,15 +1,17 @@
 package com.ljf.bean;
 
+import lombok.Data;
+
 /**
  * @author: ljf
  * @create: 2021-03-16 16:29
  * @description:
  **/
-
+@Data
 public class Emp {
     private int id;
     private String name;
     private byte age;
     private int deptId;
-    private int empno;
+    private int empNo;
 }
