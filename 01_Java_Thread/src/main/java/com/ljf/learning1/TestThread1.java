2fb3e841ddf341233956810794a3a6c7fb38c36b
ljf <ljfsunlight@gmail.com>
2021-03-19 17:55:49 +0800

    线程执行

--- 01_Java_Thread/src/main/java/com/ljf/learning1/TestThread1.java
+++ 01_Java_Thread/src/main/java/com/ljf/learning1/TestThread1.java
@@ -3,16 +3,22 @@ package com.ljf.learning1;
 /**
  * @author: ljf
  * @create: 2021-03-18 18:37
- * @description:
- *
- *
- * 学习:
+ * @description: 学习:
  * 多线程同步下载网络图片
- *
  **/
-public class TestThread1 {
+public class TestThread1 implements Runnable {
 
     public static void main(String[] args) {
+        TestThread1 testThread1 = new TestThread1();
+        new Thread(testThread1).start();
+        new Thread(testThread1).start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + "执行线程"+i);
+        }
 
     }
 }
