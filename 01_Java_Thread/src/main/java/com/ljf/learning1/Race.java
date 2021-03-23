75c3e6eda2ccc829f92e2eb64a20ee9e296167de
lijiafang <ljfsunlight@gmail.com>
2021-03-23 22:48:05 +0800

    龟兔赛跑实现线程结果

---
+++ 01_Java_Thread/src/main/java/com/ljf/learning1/Race.java
@@ -0,0 +1,61 @@
+package com.ljf.learning1;
+
+/**
+ * @author: ljf
+ * @create: 2021/3/23 22:11
+ * @description:
+ *
+ * 龟兔赛跑
+ **/
+public class Race implements Runnable {
+
+    private static String winner = null;
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i <= 100; i++) {
+
+
+            if (Thread.currentThread().getName().equals("兔子") && i%10==0){
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            boolean flag = gameOver(i);
+            if (flag){
+                break;
+            }
+            System.out.println(Thread.currentThread().getName()+"跑了"+i+"步");
+        }
+
+    }
+
+    private boolean gameOver(int i) {
+        if (winner != null){
+            return true;
+        }else{
+            if (i == 100){
+                winner = Thread.currentThread().getName();
+                System.out.println("胜利者是"+winner);
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Race race = new Race();
+        new Thread(race,"兔子").start();
+        new Thread(race,"乌龟").start();
+
+    }
+
+
+}
