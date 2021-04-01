ba77d382627e89980467dc30acdb173491f544f3
ljf <ljfsunlight@gmail.com>
2021-04-01 12:50:36 +0800

    新module参数校验

---
+++ 73_Spring_Mybatis_Validator/src/main/java/com/ljf/controller/QsTaskQueueController.java
@@ -0,0 +1,21 @@
+package com.ljf.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author ljf
+ * @since 2021-03-25
+ *
+ * 参数校验,并将校验结果返回'
+ */
+@RestController
+@RequestMapping("/project/qs-task-queue-po")
+public class QsTaskQueueController {
+
+}
