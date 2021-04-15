bc141b2b3e8ac50908200fe3a19a5a873a9708d3
ljf <ljfsunlight@gmail.com>
2021-04-15 11:31:45 +0800

    依赖注入

---
+++ 51_Spring_IOC/src/main/java/com/ljf/demo1/Test1.java
@@ -0,0 +1,12 @@
+package com.ljf.demo1;
+
+/**
+ * @author: ljf
+ * @create: 2021-04-15 11:30
+ * @description:
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        // 逻辑等同于demo0
+    }
+}
