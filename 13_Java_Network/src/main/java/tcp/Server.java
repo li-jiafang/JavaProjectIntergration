92faf4460c7bf667acee960e826b9289ff6381de
ljf <ljfsunlight@gmail.com>
2021-04-16 17:17:16 +0800

    TCP编程

---
+++ 13_Java_Network/src/main/java/tcp/Server.java
@@ -0,0 +1,67 @@
+package tcp;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+
+/**
+ * @author: ljf
+ * @create: 2021-04-16 16:56
+ * @description:
+ **/
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(6666); // 监听指定端口
+        System.out.println("server is running...");
+        for (;;) {
+            Socket sock = ss.accept();
+            System.out.println("connected from " + sock.getRemoteSocketAddress());
+            Thread t = new Handler(sock);
+            t.start();
+        }
+    }
+}
+
+class Handler extends Thread{
+    Socket socket;
+
+    public Handler(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream input = this.socket.getInputStream()) {
+            try (OutputStream output = this.socket.getOutputStream()) {
+                handle(input, output);
+            }
+        } catch (Exception e) {
+            try {
+                this.socket.close();
+            } catch (IOException ioe) {
+            }
+            System.out.println("client disconnected.");
+        }
+    }
+
+    private void handle(InputStream input, OutputStream output) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        writer.write("hello\n");
+        writer.flush();
+        for (;;) {
+            String s = reader.readLine();
+            if (s.equals("bye")) {
+                writer.write("bye\n");
+                writer.flush();
+                break;
+            }
+            writer.write("ok: " + s + "\n");
+            writer.flush();
+        }
+    }
+}
+
