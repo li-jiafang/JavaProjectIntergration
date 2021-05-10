cef82f067ca0b8d04e9af68839c0d45b3687c34c
ljf <ljfsunlight@gmail.com>
2021-05-10 18:58:45 +0800

    编程思想  内部类

---
+++ 30_Think_In_Java/src/main/java/innerClass10/Parcel1.java
@@ -0,0 +1,33 @@
+package innerClass10;
+
+/**
+ * @author: ljf
+ * @create: 2021/5/10 17:02
+ * @description:
+ **/
+public class Parcel1 {
+
+    class Contents {
+        private int i = 11;
+        public int value() { return i; }
+    }
+    class Destination {
+        private String label;
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+        String readLabel() { return label; }
+    }
+
+    public void ship(String dest) {
+        Contents c = new Contents();
+        System.out.println(c.value());
+        Destination d = new Destination(dest);
+        System.out.println(d.readLabel());
+    }
+    public static void main(String[] args) {
+        Parcel1 p = new Parcel1();
+        p.ship("Tasmania");
+    }
+
+}
