7e550c3fcd5e025cd5d9fee2696abd07c8a4c8dc
ljf <ljfsunlight@gmail.com>
2021-05-12 11:04:19 +0800

    临时文件 用于测试

---
+++ 00_Java_Test/src/main/java/TemporaryFiles.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: ljf
+ * @create: 2021/5/12 9:57
+ * @description:
+ * 用来存放一些基本小测试
+ **/
+public class TemporaryFiles {
+
+    public static void main(String[] args) {
+
+    }
+}
