c1d4f81988e38ec42eba598b85778780f218a353
ljf <ljfsunlight@gmail.com>
2021-05-19 14:05:03 +0800

    commit

---
+++ 20_Java8/src/main/java/com/ljf/stream/StreamApi1.java
@@ -0,0 +1,24 @@
+package com.ljf.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author: ljf
+ * @create: 2021-03-17 17:23
+ * @description:
+ **/
+public class StreamApi1 {
+
+
+
+    @Test
+    void test1(){
+        List<Integer> list = Arrays.asList(1,2,3,4,5,7);
+    }
+
+
+}
